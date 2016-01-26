ad2c97942912bd951edc59400872d5b13fdd7ca0
ATmega8 <295017359@qq.com>
2016-01-26 13:12:24 +0000

    函数调用测试

--- src/main.c
+++ src/main.c
@@ -4,13 +4,36 @@
 /* FreeRTOS kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-
 void vAssertCalled( unsigned long ulLine, const char * const pcFileName );
+
 void task1(void* parameters);
+void task2(void* parameters);
+
+void __attribute__((__no_instrument_function__))  
+__cyg_profile_func_enter(void *this_func, void *call_site)  
+{  
+	(void)call_site;
+	
+	//fprintf(fp, "E%p\n", this_func);
+	printf("E%p\n", this_func);
+}
+
+void __attribute__((__no_instrument_function__))  
+__cyg_profile_func_exit(void *this_func, void *call_site)  
+{  
+	(void)call_site;
+
+	//fprintf(fp, "X%p\n", this_func);
+	printf("X%p\n", this_func);
+}
 
 int main( void )
 {
 	xTaskCreate( task1, "Task1", 200, NULL, 1, NULL);
+
+	/*task2Number = 2;
+	xTaskCreate( task2, "Task2", 200, &task2Number, 1, NULL);*/
+
 	vTaskStartScheduler();	
 
 	return 0;
@@ -18,9 +41,22 @@ int main( void )
 
 void task1(void* parameters)
 {
+
 	while(1)
 	{
-		printf("task1 running\n");
+		vTaskEndScheduler();
+		break;
+
+	}
+}
+
+void task2(void* parameters)
+{
+	const TickType_t delay = 500;
+
+	while(1)
+	{
+		vTaskDelay(delay);
 	}
 }
 
@@ -41,5 +77,6 @@ void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
 
 	taskEXIT_CRITICAL();
 }
+
 /*-----------------------------------------------------------*/
 
